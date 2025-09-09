f3feef39babc108546ebc71c663f08201657d4db
massguunn <rohadigunawan0@gmail.com>
2025-09-09 15:01:36 +0800

    mengganti database ke online

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,17 +1,17 @@
 // UNTUK LOCAL
-module.exports = {
-  multipleStatements: true,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "KERAJINAN-TENUN-DB",
-};
-
-// // UNTUK ONLINE
 // module.exports = {
 //   multipleStatements: true,
 //   host: "localhost",
-//   user: "gb_db",
-//   password: "Tb8ydMMcBiArszcr",
-//   database: "gb_db",
+//   user: "root",
+//   password: "",
+//   database: "KERAJINAN-TENUN-DB",
 // };
+
+// // UNTUK ONLINE
+module.exports = {
+  multipleStatements: true,
+  host: "localhost",
+  user: "db-tenun",
+  password: "tB7nFhBBh2dKsrf5",
+  database: "DB-TENUN",
+};
